3721758209cd0b6a2073ba130bef33e390f32b66
Hyunmin Cho <ggstargame45@koomin.ac.kr>
2023-08-05 11:22:48 +0900

    첫 아키텍쳐
    riverpod, freezed, generators 등등 포함

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Malf_Application
+# flutter version : 3.10.5
+# dart version : 3.0.6
---
+++ android/app/src/main/kotlin/com/malf/malf_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.malf.malf_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
