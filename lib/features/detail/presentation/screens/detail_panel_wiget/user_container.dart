fc5680b496ad147e5e0414261c224f09f04a780f
박준수 <jspark9703@gmail.com>
2023-08-09 14:40:06 +0900

    데이터 통신 구현완료 + userImg parshing 수정

--- lib/features/detail/presentation/screens/detail_panel_wiget/user_container.dart
+++ lib/features/detail/presentation/screens/detail_panel_wiget/user_container.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:logger/logger.dart';
 import 'package:malf_application/features/detail/data/constants.dart';
 
 import '../../../data/detail_data_provider.dart';
@@ -12,28 +13,13 @@ class UserContainer extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final jsonData = ref.watch(jsonDataProvider).jsonData.data!.first;
 
-    String extractFileName(String input) {
-      // 정규표현식을 사용하여 ".jpeg"로 끝나는 문자열을 추출합니다.
-      RegExp regExp = RegExp(r'\"(.+\.jpeg)\"');
-      Match? match = regExp.firstMatch(input);
-
-      if (match != null && match.groupCount > 0) {
-        return match.group(1)!; // 첫 번째 캡처 그룹의 내용을 반환합니다.
-      } else {
-        return ''; // 일치하는 내용이 없으면 빈 문자열을 반환합니다.
-      }
-    }
-
-    Logger().d(Url.baseUrl +
-        Url.detailUrl +
-        extractFileName(jsonData!.authorpicture!));
     return Container(
       padding: const EdgeInsets.only(top: 16, bottom: 20),
       child: Row(
         children: [
           CircleAvatar(
             backgroundImage: NetworkImage(
-                "${Url.baseUrl}/${extractFileName(jsonData.authorpicture!)}"),
+                "${Url.baseUrl}/${jsonDecode(jsonData!.authorpicture!)[0]}"),
           ),
           const SizedBox(
             width: 8,
