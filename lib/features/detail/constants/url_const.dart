acc61135745a3918503ac9718584548b63d7f143
hyunmin <ggstargame45@kookmin.ac.kr>
2023-09-22 14:47:25 +0900

    일부

--- lib/features/detail/constants/url_const.dart
+++ lib/features/detail/constants/url_const.dart
@@ -1,4 +1,4 @@
 class Url {
-  static const String baseUrl = "http://3.36.185.179:8000";
+  static const String baseUrl = "http://malftravel.com";
   static const String detailUrl = "/bulletin-board/posts/";
 }
