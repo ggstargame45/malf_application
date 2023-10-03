25791cbd055bfe8a0f651dcdde2d5e50573b3a93
박준수 <jspark9703@gmail.com>
2023-10-03 15:22:33 +0900

    뒤로가기 활성화, 채팅방 정보창 토글, 키패드 딴곳누르면 비활성화

--- lib/features/chat/screens/chatting_screen.dart
+++ lib/features/chat/screens/chatting_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
@@ -74,86 +75,100 @@ class _ChattingScreenState extends State<ChattingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading:
-            const Icon(Icons.arrow_back_ios_new_outlined, color: Colors.black),
-        title: const Center(
-            child: Text(
-          "경복궁 같이가요!",
-          style: TextStyle(
-              fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),
-        )),
-        actions: const [
-          Icon(Icons.menu_rounded, color: Colors.black),
-          SizedBox(
-            width: 16,
-          )
-        ],
-      ),
-      body: Stack(
-        children: [
-          Container(
-            height: 100,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(10)),
-            ),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    width: 80,
-                    height: 80,
-                    decoration: const BoxDecoration(
-                        color: Colors.black12,
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                  ),
-                  const SizedBox(
-                    width: 300,
-                    height: 80,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              Text("sophie"),
-                            ],
-                          ),
-                          Text("경복궁 같이 가요!",
-                              style: TextStyle(
-                                  fontSize: 15, fontWeight: FontWeight.bold)),
-                          Row(
-                            children: [Text("성동구 왕십리")],
-                          )
-                        ]),
-                  )
-                ]),
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios_new, color: Colors.black),
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
-          Positioned(
-            child: Container(
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 99,
-                  ),
-                  Expanded(
-                      child: Messages(
-                    postId: widget.postId!,
-                    userName: widget.userName,
-                  )),
-                  NewMessage(
-                    userName: widget.userName,
-                    socket: _socket,
-                    postId: widget.postId,
-                  ),
-                ],
+          title: const Center(
+              child: Text(
+            "경복궁 같이가요!",
+            style: TextStyle(
+                fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),
+          )),
+          actions: const [
+            Icon(Icons.menu_rounded, color: Colors.black),
+            SizedBox(
+              width: 16,
+            )
+          ],
+        ),
+        body: Stack(
+          children: [
+            Positioned(
+              child: Container(
+                child: Column(
+                  children: [
+                    Expanded(
+                        child: Messages(
+                      postId: widget.postId!,
+                      userName: widget.userName,
+                    )),
+                    NewMessage(
+                      userName: widget.userName,
+                      socket: _socket,
+                      postId: widget.postId,
+                    ),
+                  ],
+                ),
               ),
             ),
-          )
-        ],
+            KeyboardVisibilityBuilder(builder: (context, iskeyboardVisible) {
+              return Visibility(
+                visible: !iskeyboardVisible,
+                child: Container(
+                  height: 100,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.vertical(bottom: Radius.circular(10)),
+                  ),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          width: 80,
+                          height: 80,
+                          decoration: const BoxDecoration(
+                              color: Colors.black12,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                        ),
+                        const SizedBox(
+                          width: 300,
+                          height: 80,
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text("sophie"),
+                                  ],
+                                ),
+                                Text("경복궁 같이 가요!",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold)),
+                                Row(
+                                  children: [Text("성동구 왕십리")],
+                                )
+                              ]),
+                        )
+                      ]),
+                ),
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
